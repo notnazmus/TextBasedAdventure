1e5f4237ff60d8eca934444783ef5a5e61a32861
BT_1N3_26 <nsakib6685@bths.edu>
2018-11-02 09:50:48 -0400

    Update 11/2/18 9:50AM

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,33 @@
 public class Board {
 
-    private String[][] gameBoard;
+    private Room[][] gameBoard;
 
-    public Board(String[][] gameBoard)
+    public Board(Room[][] gameBoard)
     {
         this.gameBoard = gameBoard;
     }
 
+    public Board (int height,int width)
+    {
+        this.gameBoard = new Room[height][width];
+    }
 
+    public void editBoard(int row, int col, Room room)
+    {
+        gameBoard[row][col] = room;
+    }
+
+    public void printBoard()
+    {
+        for (int i = 0; i < gameBoard.length; i++)
+        {
+            for (int n = 0; n <gameBoard[i].length;n++)
+            {
+                System.out.print(gameBoard[i][n]);
+            }
+            System.out.print("");
+        }
+    }
 
 
 
--- src/Items/Blade_Of_The_Ruined_King.java
+++
@@ -1,9 +0,0 @@
-package Items;
-
-public class Blade_Of_The_Ruined_King  extends Items{
-
-    public Blade_Of_The_Ruined_King()
-    {
-        super("Blade of The Ruined King",001);
-    }
-}
