83ffc8eb69c78f8bc71c518aff4fa8ee7630a480
BT_1N3_26 <nsakib6685@bths.edu>
2018-11-02 09:53:58 -0400

    Update 11/2/18 9:53AM

---
+++ src/Room.java
@@ -0,0 +1,20 @@
+import Items.Items;
+import Monsters.Monster;
+
+public class Room {
+
+    private Monster m;
+    private Items reward;
+    private boolean isCompleted = false;
+
+
+    public Room(Monster m, Items reward, boolean isCompleted)
+    {
+        this.m = m;
+        this.reward = reward;
+        this.isCompleted = isCompleted;
+    }
+
+    
+
+}
